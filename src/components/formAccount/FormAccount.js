928d8e7625349678969fc450c1c651a51d5a69f4
Edmilson Silva <56802572+DevEddi@users.noreply.github.com>
2022-05-28 20:01:37 -0300

    logic open account

--- src/components/formAccount/FormAccount.js
+++ src/components/formAccount/FormAccount.js
@@ -3,37 +3,65 @@ import { useState } from 'react';
 import {Picker} from '@react-native-picker/picker';
 import Slider from '@react-native-community/slider';
 
-import FormResult from '../formAccount/formResult/FormResult.js'
-
 import { 
   Text,
   View,
   TextInput,
   Button,
+  Alert,
   StyleSheet,
   TouchableOpacity,
   Switch,
 } from 'react-native';
 
 
-
-
 export default function FormAccount() {
 
-  const [name, setName] = useState();
-  const [age, setAge] = useState();
-  const [selectedGender, setSelectedGender] = useState();
+  const [name, setName] = useState(null);
+  const [age, setAge] = useState(null);
+  const [selectedGender, setSelectedGender] = useState(null);
   const [limitAccount, setLimitAccount] = useState(0);  
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
-  function returnFormComplete(){
-    setName()
-    setAge()
-    setSelectedGender()
-    setLimitAccount()
+  const showAlert = (name, age, selectedGender, limitAccount, enable) =>
+  Alert.alert(
+    "Criar conta",
+        'Nome: ' + name + '\n' +
+        'Age: ' + age + '\n' + 
+        'Gender: ' + selectedGender + '\n' +
+        'Limit: ' + limitAccount + '\n' +
+        'Student: ' + enable,
+    [
+      {
+        text: "Open Account",
+        onPress: () => Alert.alert("Thanks Open Account"),
+        style: "cancel",
+      },
+      {
+        text: "Cancel",
+        onPress: () => Alert.alert("Cancel Open Account"),
+        style: "cancel",
+      },
+    ]    
+  );
+
+  function validetionFormOpenAccount(){
+      if (name != null && age != null &&  selectedGender != null && limitAccount != 0){
+            showAlert(name, age, selectedGender, limitAccount, isEnabled)
+      }else{
+          Alert.alert('Você deve preencher todos os campos ausentes')
+      }
+
   }
 
+  function resetOpenAccount(){
+      setName(null)
+      setAge(null)
+      setSelectedGender(null)
+      setLimitAccount(0)
+      setIsEnabled(false)
+  }
 
   return (    
     <View>
@@ -89,28 +117,17 @@ export default function FormAccount() {
           </View>
 
           <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              returnFormComplete()
-            }}
-          >
+              style={styles.button}
+              onPress={() => {validetionFormOpenAccount()}}>
             <Text>Open Account</Text>  
           </TouchableOpacity>
 
           <TouchableOpacity
             style={styles.button}
-          >
+            onPress={() => {resetOpenAccount()}}>
             <Text>Reset Values</Text>  
           </TouchableOpacity>
-
-      </View>
-      <Text>
-          {name}
-          {age}
-          {selectedGender}
-          {limitAccount}
-          {isEnabled}
-      </Text>      
+      </View>          
     </View>
   );
 }
--- src/screens/AccountOpening.js
+++ src/screens/AccountOpening.js
@@ -14,8 +14,7 @@ export default function AccountOpening() {
 
   return (    
     <View>
-      <FormAccount
-      
+      <FormAccount      
       />
     </View>
   );
