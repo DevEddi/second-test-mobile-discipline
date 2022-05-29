5419086c4b2f027b87fa90a4417e89130abf01db
Edmilson Silva <56802572+DevEddi@users.noreply.github.com>
2022-05-29 17:54:14 -0300

    Animated create

--- src/components/buttonTheme/ButtonTheme.js
+++ src/components/buttonTheme/ButtonTheme.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   Text,
   View,
   TouchableOpacity,
+  Animated,
 } from 'react-native';
 
 import styles from './style'
 
-export default function ButtonTheme(props) {
+export default function ButtonTheme(props) { 
+  
+  const [largura, setLargura] = useState(new Animated.Value(100))
+
+  Animated.timing(
+    largura,
+    {
+      toValue: 410,
+      duration:4000,
+      useNativeDriver: false,
+    }
+  ).start();
+
   return (
-    <View>
+    <Animated.View
+    style={{
+      width: largura
+    }}>
       <TouchableOpacity 
         style={styles.buttonTouch}       
         onPress={props.screenDirection}      
       >
         <Text>{props.nameButton}</Text>
       </TouchableOpacity>
-    </View>
+    </Animated.View>
   );
 }
--- src/components/textInfo/TextInfo.js
+++ src/components/textInfo/TextInfo.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   Text,
   View,
+  Animated,
 } from 'react-native';
 
 import styles from './style'
 
 export default function TextInfo(props) {
+
+  const [largura, setLargura] = useState(new Animated.Value(0))
+
+  Animated.timing(
+    largura,
+    {
+      toValue: 380,
+      duration:4000,
+      useNativeDriver: false,
+    }
+  ).start();
+
   return (
-    <View>
+    <Animated.View
+      style={{
+      width: largura
+      }}>
         <Text style={styles.textInfo}>{props.information}</Text>
-    </View>
+    </Animated.View>
   );
 }
